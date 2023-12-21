f830ba4a7c38d6005aa2db14863186833c77e4c3
PySecJS <151898984+PySecJS@users.noreply.github.com>
2023-12-21 07:40:02 +0530

    Delete Hackerrank directory

--- Hackerrank/1. loop.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Enter your number : ")) 
-
-for i in range(0, n+1):   
-    i = i * i     
-    print(i)
-
--- Hackerrank/codecheff.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-for i in range(n):
-    
-    x,y=map(int,input().split())
-    if x+y > 6 : 
-        print("YES")
-    else : 
-        print("NO")
-        
